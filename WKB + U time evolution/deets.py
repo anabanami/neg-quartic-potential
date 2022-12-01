ceaa40e59773cc71097819bb8809b0612f824016
anabanami <ana.fabelah@gmail.com>
2022-12-02 05:15:42 +1100

    added WB_compendium.pdf

--- WKB + U time evolution/deets.py
+++ WKB + U time evolution/deets.py
@@ -12,13 +12,10 @@ x = np.linspace(-10, 10, Nx)
 x[x==0] = 1e-200
 delta_x = x[1] - x[0]
 
-y = np.linspace(-1, 1, Nx).T
-
 E = 15 # doesn't work for all Es
 ϵ = 0
 
-δminus = 0.3
-δplus = 0.8
+
 
 wkb = np.zeros(Nx, dtype=complex)
 x0 = np.sqrt(E)
@@ -26,37 +23,32 @@ a = -x0
 b = x0
 F0 = -(2 * a)
 F1 = 2 * b
+
+δminus = 0.3
+δplus = 0.8
        
 Q = np.sqrt((V(x, ϵ) - E).astype(complex))
 P = np.sqrt((E - V(x, ϵ)).astype(complex))
 
 u1 = F0**(1/3) * (a - x[(a - δminus < x) & (x < a + δplus)])
 
-# print(f"\n{np.shape(wkb)=}")
 # LHS of potential barrier
 integral_left = np.cumsum(Q[x < a - δminus]) * delta_x
 integral_left = -(integral_left - integral_left[-1])
 wkb[x < a - δminus] = np.exp(-integral_left) / (3 * np.sqrt(Q[x < a - δminus]))
-# print(f"\n{np.shape(wkb)=}")
 
 # approaching left turning point (from both sides of a)
 Ai_a, Aip_a, Bi_a, Bip_a = special.airy(u1)
 wkb[(a - δminus < x) & (x < a + δplus)] = Ai_a * np.sqrt(np.pi) / F0 ** (1/6)
-# print(f"\n{np.shape(wkb)=}")
 
 # inside potential barrier 
-# since ψ1 = c ψ2
-# get one array for ψ1 and one for ψ2 and divide them (make sure you avoid zero division!)
-# print("===================== ")
-# print(f"NEW INDEXING")
-excessively_long_array = np.cumsum(P[x > a]) * delta_x
+np.cumsum(P[x > a]) * delta_x
 integral_a_x = excessively_long_array[x[x > a] > a + δplus]
 
 wkb[x > a + δplus] = np.cos(integral_a_x - np.pi/4) / np.sqrt(P[x > a + δplus]) 
 
 # wkb[x > a + δplus] = (np.cos(integral_a_x) + np.sin(integral_a_x)) / 2  # indexing is correct... now my maths are dumb?
-# print(f"\n{np.shape(wkb)=}")
-# print("===================== ")
+
 
 plt.plot(x, wkb)
 plt.axvline(a, linestyle="--", color="red")
