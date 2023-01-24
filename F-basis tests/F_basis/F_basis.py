e3369ab29f502c27c96bece8b91eb82216a5bb08
anabanami <ana.fabelah@gmail.com>
2023-01-24 12:40:05 +1100

    F_split step first order works for HO->HO quench

---
+++ F-basis tests/F_basis/F_basis.py
@@ -0,0 +1,145 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+from scipy.integrate import trapezoid
+from scipy import linalg
+from tqdm import tqdm
+
+plt.rcParams['figure.dpi'] = 200
+np.set_printoptions(linewidth=200)
+
+
+def F_basis_vector(x, n):
+    return (1 / np.sqrt(P)) * np.exp(1j * 2 * np.pi * n * x / P)
+
+def Hamiltonian(x, n):
+    return (1/2) * (-((1j * 2 * np.pi * n / P) ** 2) + (x / l1**2) ** 2)
+    # return (1/2) * (-((1j * 2 * np.pi * n / P) ** 2) + (x / l2**2) ** 2)
+
+def element_integrand(x, m, n):
+    s_m = F_basis_vector(x, m)
+    s_n = F_basis_vector(x, n)
+    return np.conj(s_m) * Hamiltonian(x, n) * s_n
+
+# NxN MATRIX
+def Matrix(N):
+    M = np.zeros((N, N), dtype="complex")
+    for m in tqdm(range(N)):
+        for n in tqdm(range(N)):
+            y = element_integrand(xs, m, n)
+            element = trapezoid(y, xs, delta_x)
+            M[m][n] = element
+    return M
+
+
+def spatial_wavefunctions(N, x, evals, evects):
+    #calculating basis functions
+    x[x == 0] = 1e-200
+    s_ns = []
+    for n in range(N):
+        s_n = F_basis_vector(x, n)
+        s_ns.append(s_n)
+    s_ns = np.array(s_ns)
+
+    eigenfunctions = []
+    # print(f"{np.shape(evects) = }\n")
+
+    for j in range(5):
+        c = evects[:, j]
+        # print(f"{c= }\n")
+
+        S_jx = np.zeros(x.shape, complex)
+        # print(f"{np.shape(psi_jx) = }\n")
+
+        # for each Fourier basis vector
+        for n in range(N):
+            S_jx += c[n] * s_ns[n]
+            # print(f"{S_jx = }\n")
+
+        eigenfunctions.append(S_jx / np.max(np.abs(S_jx)))
+
+    for i in range(5):
+        ax = plt.gca()
+        color = next(ax._get_lines.prop_cycler)['color']
+
+        plt.plot(
+            x,
+            np.real(eigenfunctions[i]) + evals[i],
+            "-",
+            linewidth=1,
+            label=fR"$\psi_{i}$",
+            color=color,
+        )
+        plt.plot(
+            x, np.imag(eigenfunctions[i]) + evals[i], 
+            "--", linewidth=1, 
+            color=color
+        )
+
+        # # probability density
+        # plt.plot(
+        #     x,
+        #     abs(eigenfunctions[i] **2 ) + evals[i],
+        #     linewidth=1,
+        #     label=fR"$|\psi_{i}^2|$",
+        #     color=color,
+        # )
+        # plt.ylabel(r'$ |\psi_{n}|^2$')
+        
+
+    textstr = '\n'.join(
+        (
+            fr'$E_0 = {evals[0]:.03f}$',
+            fr'$E_1 = {evals[1]:.03f}$',
+            fr'$E_2 = {evals[2]:.03f}$',
+            fr'$E_3 = {evals[3]:.03f}$',
+            fr'$E_4 = {evals[4]:.03f}$',
+        )
+    )
+    # place a text box in upper left in axes coords
+    ax.text(0.02, 0.98, textstr, transform=ax.transAxes, verticalalignment='top')
+
+    plt.plot(x, (1/2) * (x / l1**2) ** 2, alpha=0.4, color="black")
+    # plt.plot(x, (1/2) * (x / l2**2) ** 2, alpha=0.4, color="black")
+    plt.axvline(0, linestyle=":", alpha=0.4, color="black")
+    plt.legend(loc="upper right")
+    plt.xlabel(r'$x$')
+    plt.show()
+    return s_ns
+
+
+################################################################
+## GLOBALS
+## natural units according to wikipedia
+hbar = 1
+m = 1
+ω = 1
+#lengths for HO quench
+l1 = np.sqrt(hbar / (m * ω))
+l2 = 2 * l1
+
+N = 300
+
+L = 10
+P = L
+
+Nx = 2048
+xs = np.linspace(-L / 2, L / 2, Nx)
+delta_x = xs[1] - xs[0]
+
+matrix1 = Matrix(N)
+print(np.shape(matrix1))
+# np.save(f"matrix_300_HO_1.npy", matrix)
+# np.save(f"matrix_300_HO_2.npy", matrix)
+# matrix1 = np.load(f"matrix_300_HO_1.npy")
+# matrix2 = np.load(f"matrix_300_HO_2.npy")
+
+# remember that evects are columns!
+evals1, evects1 = linalg.eigh(matrix1)
+# evals2, evects2 = linalg.eigh(matrix2)
+
+s_ns1 = spatial_wavefunctions(N, xs, evals1, evects1)
+# s_ns2 = spatial_wavefunctions(N, xs, evals2, evects2)
+
+
