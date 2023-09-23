f378deddf3d95fe34fc47f5cc0e3ee50283f1d0e
anabanami <ana.fabelah@gmail.com>
2023-09-23 11:38:03 +1000

    saving wavefunctions from JMH.c (NOT normalised). PLOTTING THEM REQUIRES THEM TO BE NORMALISED

--- evects&evals/RK4/JMH.c
+++ evects&evals/RK4/JMH.c
@@ -5,8 +5,7 @@
 //         11.802434,
 //         18.458819,
 //         25.791792,
-//     ] (edited) 
-
+//     ]
 
 // This code includes standard libraries. stdio.h is for input/output functions like printf.
  // math.h provides mathematical functions. complex.h provides support for complex numbers.
@@ -24,7 +23,7 @@
 
 // Function prototypes are declared so that they can be used before their definitions.
 void rkintegrate(ldc x0, ldc *y, ldc *z, long double h,
-                 long double E, long double theta, FILE *output);
+                 long double E, long double theta, char *output_filename);
 ldc F(long double r, long double theta, ldc psi, ldc psiprime, long double E);
 ldc G(long double r, long double theta, ldc psi, ldc psiprime, long double E);
 long double shoot(long double E1, long double E2, long double A1, long double A2);
@@ -37,7 +36,7 @@ long double shoot(long double E1, long double E2, long double A1, long double A2
 // solutions from two starting energies (E1 and E2).
 int main(void) 
 {
-    long double E1 = 0, E2 = 2.0, tempE;
+    long double E1 = 1, E2 = 1.5, tempE;
     long double three = 3.0;
     long double stepsize = 0.0005;
     long double theta_left, theta_right;
@@ -51,7 +50,8 @@ int main(void)
     
     int i, loop;
 
-    FILE *output;
+    // Save the initial energy bounds for use in output filenames
+    long double E1_init = E1, E2_init = E2;
 
     x_right = 10.0 * cexpl(I * pi * ((4.0 * 1.0 - epsilon) / (4.0 + 2.0 * epsilon)));
     x_left = 10.0 * cexpl(I * pi * ((4.0 * -2.0 - epsilon) / (4.0 + 2.0 * epsilon)));
@@ -61,8 +61,6 @@ int main(void)
 
     loop = (int) (x_right / stepsize);
     loop++;
-
-    output = fopen("output", "w");
 
     psi1_right = 1.0;
     psiprime1_right = -cpowl(I, epsilon) * cpowl(x_right, (2.0 + epsilon)) * psi1_right;
@@ -76,44 +74,54 @@ int main(void)
 
     for (i = 0; i < 40; i++)
     {
-    rkintegrate(x_right, &psi1_right, &psiprime1_right, -stepsize, E1, theta_right, output);
-    rkintegrate(x_right, &psi2_right, &psiprime2_right, -stepsize, E2, theta_right, output);
-    rkintegrate(x_left, &psi1_left, &psiprime1_left, -stepsize, E1, theta_left, output);
-    rkintegrate(x_left, &psi2_left, &psiprime2_left, -stepsize, E2, theta_left, output);
+        rkintegrate(x_right, &psi1_right, &psiprime1_right, -stepsize, E1, theta_right, NULL);
+        rkintegrate(x_right, &psi2_right, &psiprime2_right, -stepsize, E2, theta_right, NULL);
+        rkintegrate(x_left, &psi1_left, &psiprime1_left, -stepsize, E1, theta_left, NULL);
+        rkintegrate(x_left, &psi2_left, &psiprime2_left, -stepsize, E2, theta_left, NULL);
 
-    psi1_diff = cexpl(-I * theta_left) * (psiprime1_left / psi1_left);
-    psi1_diff -= cexpl(-I * theta_right) * (psiprime1_right / psi1_right);
-    psi2_diff = cexpl(-I * theta_left) * (psiprime2_left / psi2_left);
-    psi2_diff -= cexpl(-I * theta_right) * (psiprime2_right / psi2_right);
+        psi1_diff = cexpl(-I * theta_left) * (psiprime1_left / psi1_left);
+        psi1_diff -= cexpl(-I * theta_right) * (psiprime1_right / psi1_right);
+        psi2_diff = cexpl(-I * theta_left) * (psiprime2_left / psi2_left);
+        psi2_diff -= cexpl(-I * theta_right) * (psiprime2_right / psi2_right);
 
-    printf("%d\t\t%.10Lf\t\t%.10Lf\n", i + 1, E1, E2);
+        printf("%d\t\t%.10Lf\t\t%.10Lf\n", i + 1, E1, E2);
 
-    if (!isnan(E1))
-    {
-        tempE = E1;
-        E1 = shoot(E1, E2, cabsl(psi1_diff), cabsl(psi2_diff));
-        E2 = tempE;
-    } 
+        if (!isnan(E1))
+        {
+            tempE = E1;
+            E1 = shoot(E1, E2, cabsl(psi1_diff), cabsl(psi2_diff));
+            E2 = tempE;
+        } 
 
-    else {
-        printf("%.20Lf\n", E2);
-        return 0;
+        else 
+        {
+            printf("%.20Lf\n", E2);
+            return 0;
+        }
+
+        psi1_right = 1.0;
+        psiprime1_right = -cpowl(I, epsilon) * cpowl(x_right, (2.0 + epsilon)) * psi1_right;
+        psi2_right = 1.0;
+        psiprime2_right = -cpowl(I, epsilon) * cpowl(x_right, (2.0 + epsilon)) * psi2_right;
+        
+        psi1_left = 1.0;
+        psiprime1_left = -cpowl(I, epsilon) * cpowl(x_left, (2.0 + epsilon)) * psi1_left;
+        psi2_left = 1.0;
+        psiprime2_left = -cpowl(I, epsilon) * cpowl(x_left, (2.0 + epsilon)) * psi2_left;
     }
 
-    psi1_right = 1.0;
-    psiprime1_right = -cpowl(I, epsilon) * cpowl(x_right, (2.0 + epsilon)) * psi1_right;
-    psi2_right = 1.0;
-    psiprime2_right = -cpowl(I, epsilon) * cpowl(x_right, (2.0 + epsilon)) * psi2_right;
-    
-    psi1_left = 1.0;
-    psiprime1_left = -cpowl(I, epsilon) * cpowl(x_left, (2.0 + epsilon)) * psi1_left;
-    psi2_left = 1.0;
-    psiprime2_left = -cpowl(I, epsilon) * cpowl(x_left, (2.0 + epsilon)) * psi2_left;
-}
+    // Integrate once more, to save the results to output csv files
+    char output_filename[256];
 
-printf("%.20Lf\n", E1);
+    sprintf(output_filename, "psi_left_%.1Lf-%.1Lf.csv", E1_init, E2_init);
+    rkintegrate(x_left, &psi1_left, &psiprime1_left, -stepsize, E1, theta_left, output_filename);
 
-return 0;
+    sprintf(output_filename, "psi_right_%.1Lf-%.1Lf.csv", E1_init, E2_init);
+    rkintegrate(x_right, &psi1_right, &psiprime1_right, -stepsize, E1, theta_right, output_filename);
+
+    printf("%.20Lf\n", E1);
+
+    return 0;
 
 }
 
@@ -121,7 +129,7 @@ return 0;
 // for solving ordinary differential equations. The method is used to numerically integrate
 // the differential equations defined by functions F and G.
 void rkintegrate(ldc x0, ldc *y, ldc *z, long double h,
-                 long double E, long double theta, FILE *output)
+                 long double E, long double theta, char *output_filename)
 {
     ldc k1, k2, k3, k4;
     ldc l1, l2, l3, l4;
@@ -134,25 +142,52 @@ void rkintegrate(ldc x0, ldc *y, ldc *z, long double h,
     r = cabsl(x0);
     psi = *y; psiprime = *z;
 
+    FILE *output;
+    if (output_filename != NULL)
+    {
+        output = fopen(output_filename, "w");
+        fprintf(output, "r,psi_real,psi_imag\n");
+    }
+
     for(loop = 0; loop < 20000; loop++)
     {
-    k1 = h * F(r, theta, psi , psiprime, E);
-    l1 = h * G(r, theta, psi , psiprime, E);
+        k1 = h * F(r, theta, psi , psiprime, E);
+        l1 = h * G(r, theta, psi , psiprime, E);
 
-    k2 = h * F(r + h * 0.5, theta, psi + k1 * 0.5, psiprime + l1 * 0.5 , E);
-    l2 = h * G(r + h * 0.5, theta , psi + k1 * 0.5, psiprime + l1 * 0.5 , E) ;
+        k2 = h * F(r + h * 0.5, theta, psi + k1 * 0.5, psiprime + l1 * 0.5 , E);
+        l2 = h * G(r + h * 0.5, theta , psi + k1 * 0.5, psiprime + l1 * 0.5 , E) ;
 
-    k3 = h * F(r + h * 0.5, theta, psi + k2 * 0.5, psiprime + l2 * 0.5, E) ;
-    l3 = h * G(r + h * 0.5, theta, psi + k2 * 0.5, psiprime + l2 * 0.5, E) ;
+        k3 = h * F(r + h * 0.5, theta, psi + k2 * 0.5, psiprime + l2 * 0.5, E) ;
+        l3 = h * G(r + h * 0.5, theta, psi + k2 * 0.5, psiprime + l2 * 0.5, E) ;
 
-    k4 = h * F(r + h , theta, psi + k3, psiprime + l3, E);
-    l4 = h * G(r + h , theta, psi + k3, psiprime + l3, E) ;
+        k4 = h * F(r + h , theta, psi + k3, psiprime + l3, E);
+        l4 = h * G(r + h , theta, psi + k3, psiprime + l3, E) ;
 
-    psi += (k1 + k2 + k2 + k3 + k3 + k4 ) / 6.0;
-    psiprime += (l1 + l2 + l2 + l3 + l3 + l4 ) / 6.0 ;
+        psi += (k1 + k2 + k2 + k3 + k3 + k4 ) / 6.0;
+        psiprime += (l1 + l2 + l2 + l3 + l3 + l4 ) / 6.0 ;
 
-    r += h ;
-}
+        r += h ;
+
+        if (output_filename != NULL)
+        {
+            fprintf(output, "%.10Lf,%.10Lf,%.10Lf,\n", r, creall(psi), cimagl(psi));
+        }
+
+        // //  NORMALISEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE <><><><><><><>
+        //     if (output_filename != NULL)
+        // {
+        //     long double magnitude = cabsl(psi); // calculates the magnitude of psi
+        //     if (magnitude == 0) // to avoid division by zero
+        //     {
+        //         fprintf(output, "%.10Lf,%.10Lf,%.10Lf,\n", r, 0.0L, 0.0L);
+        //     }
+        //     else
+        //     {
+        //         fprintf(output, "%.10Lf,%.10Lf,%.10Lf,\n", r, creall(psi) / magnitude, cimagl(psi) / magnitude);
+        //     }
+        // }
+
+    }
 
     *y = psi ;
     *z = psiprime;
